afe626b8bbd48079773eadb336127ff20da6b56b
Luke Bergmann <lukebergmann0@gmail.com>
2021-04-01 04:43:44 +0000

    Parameterized students.js and teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,23 @@ const pool = new Pool({
 })
 
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const queryString =`
+SELECT DISTINCT teachers.name AS teacher, cohorts.name as cohorts
 FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = $1
+GROUP BY cohorts.name, teachers.name
+ORDER BY teacher
+`
+const cohortName = [process.argv[2]];
+
+pool.query(queryString, cohortName)
+  .then(res => {
+    console.log(res.rows)
+    res.rows.forEach(user => {
+      console.log(`${user.teacher} ${user.cohorts}`);
+    })
+
+  }).catch(err => console.error('query error', err.stack));
